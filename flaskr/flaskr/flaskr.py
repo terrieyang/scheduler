ac82f5c7d36a8db823043a043ee37dafda646196
Terrie Yang <terrieyang@berkeley.edu>
2018-01-21 12:09:54 -0800

    update imports

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-from flaskr import shifts
+import shifts
 
 
 app = Flask(__name__) # create the application instance :)
